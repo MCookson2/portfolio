7a8a60dd210be121ea298deb63d5dfeecd05a93b
MCookson2 <matt.cookson2@gmail.com>
2021-05-10 22:00:18 +0100

    add js to create darkmode option and create styles for that

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,14 +1,38 @@
 let darkMode = false;
 
-const darkModeButton = document.getElementById('darkMode')
+const darkModeButton = document.getElementById('darkMode');
+const defaultBackground = document.querySelectorAll('.backgroundColour')
+const selectGrid = document.querySelectorAll('.selectorClass');
 
 darkModeButton.addEventListener('click', function(){
     if (darkMode === false) {
         darkMode = true;
-        document.body.classList.add('darkMode');
+        document.body.classList.add('darkMode')
+        for (let i = 0; i < defaultBackground.length; i++) {
+            defaultBackground[i].classList.remove('backgroundColour');
+        }
+        for (let i = 0; i < selectGrid.length; i++) {
+            if (i === selectGrid.length - 1) {
+                return
+            } else {
+                selectGrid[i].classList.add('darkMode')
+            }
+        }
     }
     else if (darkMode === true) {
         darkMode = false;
         document.body.classList.remove('darkMode');
+        for (let i = 0; i < defaultBackground.length; i++) {
+            defaultBackground[i].classList.add('backgroundColour');
+        }
+        for (let i = 0; i < selectGrid.length; i++) {
+            if (i === selectGrid.length - 1) {
+                return
+            } else {
+                selectGrid[i].classList.remove('darkMode');
+            }
+        }
     }
 })
+
+console.log(defaultBackground)
